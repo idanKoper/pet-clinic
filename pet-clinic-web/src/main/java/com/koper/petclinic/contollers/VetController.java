8dabf1b15718925ec5e1cb80c8e8a20964b460a6
idan koper <idankoper@gmail.com>
2020-03-29 16:32:13 +0300

    * Add wro4j plugin and config Spring. * Change location of static resources.

--- pet-clinic-web/src/main/java/com/koper/petclinic/contollers/VetController.java
+++ pet-clinic-web/src/main/java/com/koper/petclinic/contollers/VetController.java
@@ -18,6 +18,6 @@ public class VetController {
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
-        return "vets/index";
+        return "vets/vetsList";
     }
 }

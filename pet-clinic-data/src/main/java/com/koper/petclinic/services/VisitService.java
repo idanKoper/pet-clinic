392145481c37b0acd771fe49f0f516ed012a5437
idan koper <idankoper@gmail.com>
2020-04-04 22:34:04 +0300

    Implement map impl for Visit Service.

---
+++ pet-clinic-data/src/main/java/com/koper/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.koper.petclinic.services;
+
+import com.koper.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/koper/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,41 @@
+package com.koper.petclinic.services.map;
+
+import com.koper.petclinic.model.Visit;
+import com.koper.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.deleteByObject(visit);
+    }
+
+    @Override
+    public void deletById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid Visit");
+        else{
+            return super.save(visit);
+        }
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
